fb1a2b0c98208dd9805c2b29a8f93d3982757873
Anil Bagale <121026223+anilbagale@users.noreply.github.com>
2025-03-14 00:26:45 +0530

    Update EA - Build Entity Name.sh

--- EA - Build Entity Name.sh
+++ EA - Build Entity Name.sh
@@ -1,14 +1,4 @@
-<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<computer_extension_attribute>
-    <id>32</id>
-    <name>Build Entity</name>
-    <enabled>true</enabled>
-    <description></description>
-    <data_type>String</data_type>
-    <input_type>
-        <type>script</type>
-        <platform>Mac</platform>
-        <script>#!/bin/bash
+#!/bin/bash
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # SCRIPT INFO
@@ -43,10 +33,7 @@ else
 fi
 
 if [ -n "${entity}" ]; then
-    echo "&lt;result&gt;${entity}&lt;/result&gt;"
+    echo "<result>${entity}</result>"
 fi
 
-exit 0</script>
-    </input_type>
-    <inventory_display>Extension Attributes</inventory_display>
-</computer_extension_attribute>
+exit 0
